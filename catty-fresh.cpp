b6446b8594383a93098ca704d8bf00c0d6ee275e
Alexander von Gluck IV <kallisti5@unixzen.com>
2012-09-02 13:33:10 -0500

    catty-fresh: pull database from cattyscan.net cdn

--- catty-fresh.cpp
+++ catty-fresh.cpp
@@ -28,8 +28,47 @@
 #define CLEAN(x...) printf(" \033[37m+ Scanned:\033[0m " x)
 
 
+size_t
+writeCallback(void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+	static int first_time = 1;
+
+	char* home = getenv("HOME");
+	char configPath[PATH_MAX];
+	char databaseFile[PATH_MAX];
+
+	if (home != NULL) {
+		snprintf(configPath, PATH_MAX, "%s/.catty", home);
+		snprintf(databaseFile, PATH_MAX, "%s/vicious.db", configPath);
+	}
+
+	struct stat st;
+	if (stat(configPath, &st) != 0) {
+		if (!S_ISDIR(st.st_mode)) {
+			if (mkdir(configPath, 0755) != 0) {
+				ERROR("Couldn't create %s directory!\n", configPath);
+				return 0;
+			}
+		}
+	}
+
+	static FILE *outfile;
+	size_t written;
+	if (first_time) {
+		first_time = 0;
+		outfile = fopen(databaseFile, "w");
+		if (outfile == NULL) {
+			return -1;
+		}
+	}
+	written = fwrite(ptr, size, nmemb, outfile);
+
+	return written;
+}
+
+
 static bool
-obtainDB()
+obtainDB(char* url)
 {
 	CURL *curl = curl_easy_init();
 
@@ -38,7 +77,8 @@ obtainDB()
 		return false;
 	}
 
-	curl_easy_setopt(curl, CURLOPT_URL, "https://cdn.cattyscan.net/");
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeCallback);
 
 	// TODO: We should have our own CA to prevent man-in-the-middle
 	// curl_easy_setopt(curl, CURLOPT_CAPATH, 0L);
@@ -52,6 +92,8 @@ obtainDB()
 	}
 
 	curl_easy_cleanup(curl);
+
+	printf("Virus database updated successfully!\n");
 	return true;
 }
 
@@ -62,11 +104,9 @@ main(int argc, char* argv[])
 	printf("CattyFresh, CattyScan v%d.%d\n", VERSION_MAJOR, VERSION_MINOR);
 	printf("CattyFresh vicious software database updater\n");
 
-	char* home = getenv("HOME");
-	char databaseFile[PATH_MAX];
-	if (home != NULL)
-		snprintf(databaseFile, PATH_MAX, "%s/.catty/vicious.db", home);
 
-	obtainDB();
+	char cattyURL[] = "http://cdn.cattyscan.net";
+	obtainDB(cattyURL);
+
 	return 0;
 }
